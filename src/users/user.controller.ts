dbc9ab703562f4c987eca495bf5be9b65413387f
Kellyson Dias <kellysondias@outlook.com>
2024-08-15 14:54:26 -0300

    🚧feat:create 'update' controller prototype

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,5 +1,5 @@
-import { NextFunction, Request, Response } from 'express';
-import UsersService from './user.service';
+import { NextFunction, Request, Response } from "express";
+import UsersService from "./user.service";
 
 export class UserController {
   async create(req: Request, res: Response, next: NextFunction) {
@@ -7,20 +7,22 @@ export class UserController {
       const user = req.body;
       const result = await UsersService.create(user);
 
-      if (result && 'errors' in result) {
+      if (result && "errors" in result) {
         return res.status(400).json(result);
       }
 
       if (!result) {
         return res.status(400).json({
-          status: 'error',
-          errors: [{ field: 'email', message: 'User with this email already exists' }],
+          status: "error",
+          errors: [
+            { field: "email", message: "User with this email already exists" },
+          ],
         });
       }
 
       return res.status(201).json(result);
     } catch (error) {
-      return res.status(500).json({ status: 'error', message: error.message });
+      return res.status(500).json({ status: "error", message: error.message });
     }
   }
 
@@ -28,7 +30,7 @@ export class UserController {
     try {
       const users = await UsersService.findAll();
       if (!users) {
-        return res.status(404).send('Users not found');
+        return res.status(404).send("Users not found");
       }
       return res.status(200).json(users);
     } catch (error) {
@@ -40,15 +42,45 @@ export class UserController {
     try {
       const id = parseInt(req.params.id);
       if (isNaN(id)) {
-        return res.status(400).send('Invalid ID');
+        return res.status(400).send("Invalid ID");
       }
 
       const user = await UsersService.findById(id);
       if (!user) {
-        return res.status(404).send('User not found');
+        return res.status(404).send("User not found");
       }
 
       return res.status(200).json(user);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async update({ body, params }: Request, res: Response, next: NextFunction) {
+    try {
+      const id = params.id;
+
+      const { firstName, lastName, email, password } = body;
+
+      if (password || email) {
+        return res
+          .status(400)
+          .json({
+            message: `${
+              password ? "Password" : "E-mail"
+            } update is not allowed`,
+          });
+      }
+
+      const userData = { firstName, lastName };
+
+      //const updatedUser = await UsersService.(id, userData);
+
+      // if (!updatedUser) {
+      //   return res.status(404).send("User not found");
+      // }
+
+      return res.status(200).send("User was updated successfully");
     } catch (error) {
       next(error);
     }
